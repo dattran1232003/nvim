544cae7c1889ea31cd1c8bac6185cdc0d2c56e78
Quoc Dat <quoc-dat.tran@ant-tech.eu>
2022-01-28 13:28:34 +0700

    add support Debian: key mapping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -73,7 +73,10 @@ map('n', '<Leader>tm', ':tabmove<CR>', { noremap = true })
 map('n', '<Leader>tn', ':tabnew .<CR>', { noremap = true })
 map('n', '<Leader>n' , ':tabnext<CR>', { noremap = true })
 map('n', '<Leader>p' , ':tabprevious<CR>', { noremap = true })
-map('n', '<Leader>td', ':DuplicateTabpane<CR>', { noremap = true })
+
+-- for Debian linux
+map('n', '<C-t>', ':tabnew .<CR>', { noremap = true })
+map('n', '<C-w>', ':tabclose<CR>', { noremap = true })
 
 -- switch tab using leander + number
 map('n', '<leader>1', '1gt', { noremap=true })
