1be0f716104f9b6cd4634b39f99075c7640beb3a
Dat Tran <quoc-dat.tran@ant-tech.eu>
2022-04-11 10:28:39 +0700

    add neo runner

---
+++ lua/plugins/neo-runner.lua
@@ -0,0 +1,11 @@
+local g = vim.g
+
+-- C/C++
+g.runner_c_compiler = 'gcc'
+g.runner_cpp_compiler = 'g++'
+g.runner_c_options = '-std=c99 -Wall -o'
+g.runner_cpp_options = '-std=c++17 -Wall'
+
+-- Python
+g.runner_python_ex = 'python3'
+g.runner_python_options = ''
