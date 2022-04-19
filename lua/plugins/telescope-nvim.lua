da2d9a5e28c7f489b288e1f7de2f08ead8d9700c
Dat Tran <quoc-dat.tran@ant-tech.eu>
2022-04-19 11:36:02 +0700

    change live_grep to grep_string neovim

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -3,6 +3,7 @@ local map = vim.api.nvim_set_keymap
 -- nvim-telescope/telescope-dap.nvim
 require('telescope').load_extension('dap')
 
+
 -- key mappings
 local opts = { noremap = true, silent=true}
 
@@ -11,7 +12,8 @@ map('n', '<leader>ds', ':Telescope dap frames<CR>', opts)
 map('n', '<leader>db', ':Telescope dap list_breakpoints<CR>', opts)
 
 map('n', '<c-p>', "<cmd>Telescope find_files hidden=true<cr>", opts)
-map('n', '<c-n>', '<cmd>Telescope live_grep<cr>', opts)
+-- map('n', '<c-n>', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<c-n>', "<cmd>lua require'telescope.builtin'.grep_string{ shorten_path = true, word_match = \"-w\", only_sort_text = true, search = '' }<cr>", opts)
 map('n', 'fb', '<cmd>Telescope buffers<cr>', opts)
 map('n', 'fh', '<cmd>Telescope help_tags<cr>', opts)
 
