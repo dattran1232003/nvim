4d5cfa2b8eee0a0abec6a27c982255c2656322ca
Dat Tran <quoc-dat.tran@ant-tech.eu>
2022-04-27 09:57:23 +0700

    fix: telescope fzf

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -3,13 +3,20 @@ local telescope = require('telescope')
 
 -- nvim-telescope/telescope-dap.nvim
 telescope.setup {
+  defaults = {
+    path_display = { 'smart' }
+  },
+  pickers ={
+    grep_string = {
+      path_display = { 'shorten' }
+    }
+  },
   extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
     }
   }
 }
@@ -24,9 +31,8 @@ map('n', '<leader>ds', ':Telescope dap frames<CR>', opts)
 map('n', '<leader>db', ':Telescope dap list_breakpoints<CR>', opts)
 
 map('n', '<c-p>', "<cmd>Telescope find_files hidden=true<cr>", opts)
-map('n', '<c-n>', "<cmd>lua require'telescope.builtin'.grep_string{ shorten_path = true, word_match = \"-w\", only_sort_text = true }<cr>", opts)
-map('n', '<cr>', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find({ sorter = require('telescope.sorters').get_substr_matcher({})})<cr>", { noremap=false, silent=true })
+map('n', '<c-n>', "<cmd>lua require'telescope.builtin'.grep_string({ search='' })<cr>", opts)
+map('n', '<cr>', "<cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find({ sorter = require('telescope.sorters').get_substr_matcher({})})<cr>", { noremap=false, silent=true })
 map('n', 'fb', '<cmd>Telescope buffers<cr>', opts)
 map('n', 'fh', '<cmd>Telescope help_tags<cr>', opts)
-
 
