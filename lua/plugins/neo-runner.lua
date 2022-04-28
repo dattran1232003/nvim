2915937df4fd4d6c5fd3775d88cc3e31028e13e5
Dat Tran <quoc-dat.tran@ant-tech.eu>
2022-04-28 22:06:00 +0700

    neo runner for c++

--- lua/plugins/neo-runner.lua
+++ lua/plugins/neo-runner.lua
@@ -1,4 +1,5 @@
 local g = vim.g
+local map = vim.api.nvim_set_keymap
 
 -- C/C++
 g.runner_c_compiler = 'gcc'
@@ -9,3 +10,7 @@ g.runner_cpp_options = '-std=c++17'
 -- Python
 g.runner_python_ex = 'python3'
 g.runner_python_options = ''
+
+
+-- KEY BINDINGS
+map('n', '<F5>', ':NeoRunner<CR>', { silent=true })
