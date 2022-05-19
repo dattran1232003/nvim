24e5b0b0b8e0d6b57e6feb31a24366f7e2c1df2c
Dat Tran <dattran@pop-os.localdomain>
2022-05-19 10:10:40 +0700

    fix: diffview error

--- lua/plugins/p_diffview.lua
+++ lua/plugins/p_diffview.lua
@@ -14,19 +14,18 @@ require'diffview'.setup {
     -- fold_open = "?",
   },
   file_panel = {
-    position = "left",            -- One of 'left', 'right', 'top', 'bottom'
-    width = 35,                   -- Only applies when position is 'left' or 'right'
-    height = 10,                  -- Only applies when position is 'top' or 'bottom'
-    listing_style = "tree",       -- One of 'list' or 'tree'
+    win_config = {
+      position = "left",            -- One of 'left', 'right', 'top', 'bottom'
+      width = 35,                   -- Only applies when position is 'left' or 'right'
+      height = 10,                  -- Only applies when position is 'top' or 'bottom'
+      listing_style = "tree",       -- One of 'list' or 'tree'
+    },
     tree_options = {              -- Only applies when listing_style is 'tree'
       flatten_dirs = true,
       folder_statuses = "always"  -- One of 'never', 'only_folded' or 'always'.
     }
   },
   file_history_panel = {
-    position = "bottom",
-    width = 35,
-    height = 16,
     log_options = {
       max_count = 256,      -- Limit the number of commits
       follow = false,       -- Follow renames (only for single file)
