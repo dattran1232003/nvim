f356caa64f0e6efee2d2ff15b22897fa28d9aaeb
Trần Quốc Đạt <dattran@pop-os.localdomain>
2022-07-08 11:55:15 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Downloading lombok binary for Java"
+sudo mkdir -p /usr/local/share/lombok
 curl https://projectlombok.org/downloads/lombok.jar --output /usr/local/share/lombok/lombok.jar || echo "Download lombok failed"
 echo "Download completed"
